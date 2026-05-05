23227c3c488671d0c239c249181bf1f996417de9
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-05 15:33:29 +0200

    fix pycodestyle

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -12,4 +12,4 @@ elif lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 else:
     print(f"Last digit of {number} is {lastDigit}",
-          " and is less than 6 and not 0")
+          "and is less than 6 and not 0")
