9ec4c0e4234e3c2ca0fc68af1aec7de089d74382
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-05 10:21:52 +0200

    feat:task1

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+import math
+
+# Initialisation de pi avec 2 decimales
+Pi_approx = round(math.pi, 2)
+
+# Initialisation du booléen de validation
+if Pi_approx == 3.14:
+	valid = True
+else:
+	valid = False
 
 print("Language: Python")
 print("Version: 3")
-print(f": 3.14")
+print(f"Pi approx: {Pi_approx}")
 print("Computation valid: True")
