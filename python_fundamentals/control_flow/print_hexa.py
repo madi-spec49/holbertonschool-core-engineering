e35a87c625a30b251e9d2d8cfb5638c534eedf79
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-06 23:50:09 +0200

    task3

--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # This script prints the hexadecimal representation of numbers from 0 to 98.
 for decimal_val in range(0, 99):
-    print(f"{decimal_val} = {decimal_val:x}")
-
+    print(f"{decimal_val} = {decimal_val:#x}")
+    
