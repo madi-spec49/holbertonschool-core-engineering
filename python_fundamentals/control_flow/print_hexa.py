8e8efc9d339e37eaba03c7552b3176ab40bb24b9
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-07 10:14:05 +0200

    task3

--- python_fundamentals/control_flow/print_hexa.py
+++ python_fundamentals/control_flow/print_hexa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-decimal_val = list(range(0, 99))
 for decimal_val in range(0, 99):
 
-    print(f"{decimal_val} = {decimal_val:#x}")
+    print("{:d} = 0x{:x}".format(decimal_val,decimal_val))
+
