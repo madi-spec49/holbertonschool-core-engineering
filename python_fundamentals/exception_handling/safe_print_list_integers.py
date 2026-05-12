426bd594bec25510e35acecd423dcd57714e7cc9
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-12 17:26:00 +0200

    task2

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -5,7 +5,9 @@ def safe_print_list_integers(my_list, x):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except (ValueError, TypeError, IndexError):
-            continue
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            break
     print()
     return count
