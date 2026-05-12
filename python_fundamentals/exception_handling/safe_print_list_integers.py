ffb151767c27953827939cc086ae34d01e911fa3
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-12 17:04:51 +0200

    task2

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-
 def safe_print_list_integers(my_list, x):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-    except (ValueError, TypeError):
+        except (ValueError, TypeError, IndexError):
             pass
     print()
     return count
