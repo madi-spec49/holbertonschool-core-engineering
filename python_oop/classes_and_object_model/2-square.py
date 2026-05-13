d81af428702758388e1c74ea7642411ef0bcede4
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-13 15:07:47 +0200

    task1

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Defines a class Square.
+"""
+
+class Square:
+    """
+ Represents a square.
+    """
+    def __init__(self, size=0):
+        """Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+        """
+        self.size = size
