e5690ec6ce114416137e1606b2221d711d04d3b2
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-13 15:29:36 +0200

    task3

--- python_oop/classes_and_object_model/3-square.py
+++ python_oop/classes_and_object_model/3-square.py
@@ -7,7 +7,7 @@ Defines a class Square.
 class Square:
     """
     Represents a square with a private size attribute.
-    
+
     The size attribute is private to enforce encapsulation,
     allowing the class builder to control its type and value.
     """
@@ -15,7 +15,7 @@ class Square:
     def __init__(self, size):
         """
         Initialize a new Square instance.
-        
+
         Args:
         size: The size of the square (private instance attribute)
         """
