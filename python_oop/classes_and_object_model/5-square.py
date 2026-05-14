b9fcaf2624b74aab87cb63036a947ba8495131c3
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-14 13:15:10 +0200

    task6

--- python_oop/classes_and_object_model/5-square.py
+++ python_oop/classes_and_object_model/5-square.py
@@ -35,3 +35,22 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #.
+
+        If size is equal to 0, print an empty line.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
