24e1609f03a16f2822d19de0a33acc24a5ab800c
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-15 03:18:21 +0200

    task1

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module that defines a Rectangle class inheriting from BaseGeometry.
+"""
+
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle that inherits from BaseGeometry.
+
+    The rectangle is defined by its width and height, both of which must be
+    positive integers. Validation is performed using the integer_validator
+    method from the parent class.
+    """
+
+    def __init__(self, width, height):
+        """
+        Initialize a new Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle (must be positive integer)
+            height (int): The height of the rectangle (must be positive integer)
+
+        Raises:
+            TypeError: If width or height is not an integer
+            ValueError: If width or height is less than or equal to 0
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
