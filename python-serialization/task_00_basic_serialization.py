b48da1455d80b598262508f927ec831930bbd4d6
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-29 22:35:33 +0200

    task0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
-"""Basic serialization module that serializes a Python dictionary to JSON
-and deserializes JSON back to a Python dictionary."""
+"""Basic serialization module that adds the functionality to serialize a
+Python dictionary to a JSON file and deserialize the JSON file to recreate
+the Python Dictionary."""
 
 import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """Serializes a Python dictionary to a JSON file.
+    """Serialize a Python dictionary to a JSON file.
 
     Args:
-        data (dict): A Python dictionary with data to serialize.
+        data (dict): A Python Dictionary with data to serialize
         filename (str): The filename of the output JSON file.
-                       If the file already exists, it will be replaced.
+                       If the output file already exists it should be replaced.
     """
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(data, f)
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file)
 
 
 def load_and_deserialize(filename):
-    """Loads and deserializes data from a JSON file to a Python dictionary.
+    """Load and deserialize data from a JSON file to a Python dictionary.
 
     Args:
         filename (str): The filename of the input JSON file.
 
     Returns:
-        dict: A Python dictionary with the deserialized JSON data from the file.
+        dict: A Python Dictionary with the deserialized JSON data from the file.
     """
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
