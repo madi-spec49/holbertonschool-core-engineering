e8c84c27d096a828030a4abe823e21b8e24ed079
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-29 20:38:04 +0200

    task0

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+"""Defines a read_file function."""
+
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read(), end='')
+    """Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, encoding="utf-8") as MyFile:
+        print(MyFile.read(), end="")
