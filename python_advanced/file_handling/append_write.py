76d090f1d08340b2ba9dd18744ec7e65a1eee09b
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-29 21:14:59 +0200

    task2

---
+++ python_advanced/file_handling/append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines an append_write function."""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF-8) and returns the number of characters added."""
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
