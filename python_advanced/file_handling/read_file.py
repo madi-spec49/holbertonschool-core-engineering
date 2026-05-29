5d074c0d52de1bebd23e95f01ac7911100427e30
madi-spec49 <madi.anli.madi@gmail.com>
2026-05-29 20:32:31 +0200

    task0

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 
-def read_file(filename):
-    with open(filename, 'r') as f:
-        print(f.read())
+def read_file(filename=""):
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
