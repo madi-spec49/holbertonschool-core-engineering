074b05966c798f7edc67fbc016adbad4cbd7d637
madi-spec49 <madi.anli.madi@gmail.com>
2026-07-09 11:52:05 +0200

    websocket connection

--- python_advanced/websockets/echo_server.py
+++ python_advanced/websockets/echo_server.py
@@ -2,15 +2,18 @@
 import asyncio
 import websockets
 
-
 async def gerer_client(websocket):
     async for message in websocket:
         await websocket.send(message)
 
-
 async def main():
-    async with websockets.serve(gerer_client, "localhost", 8765):
-        await asyncio.Future()
-
+    # Essaie d'abord avec 127.0.0.1
+    try:
+        async with websockets.serve(gerer_client, "127.0.0.1", 8765):
+            await asyncio.Future()
+    except OSError:
+        # Si ça échoue, essaie avec localhost
+        async with websockets.serve(gerer_client, "localhost", 8765):
+            await asyncio.Future()
 
 asyncio.run(main())
