17e55804e9df22c4407a41e6b975ea27f7884442
madi-spec49 <madi.anli.madi@gmail.com>
2026-07-16 16:11:21 +0200

    Add websocket client implementation in ws_client.py

---
+++ python_advanced/websockets/ws_client.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import asyncio
+import websockets
+
+
+async def connect_and_send(uri: str, text: str) -> str:
+    async with websockets.connect(uri) as websocket:
+        await websocket.send(text)
+        response = await websocket.recv()
+        return response
+
+
+async def main():
+    response = await connect_and_send("ws://localhost:8765", "demo")
+    print(response, end="")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
