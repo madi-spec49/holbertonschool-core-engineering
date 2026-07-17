141930317f7a2f5da806d0498121ef62ab70d697
madi-spec49 <madi.anli.madi@gmail.com>
2026-07-17 18:25:49 +0200

    Add ASGI server implementation for WebSocket connections

---
+++ python_advanced/websockets/asgi_server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.routing import Route, WebSocketRoute
+
+
+async def homepage(request):
+    return HTMLResponse("""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>WebSocket App</title>
+    </head>
+    <body>
+        <h1>WebSocket App</h1>
+    </body>
+    </html>
+    """)
+
+
+async def websocket_endpoint(websocket):
+    await websocket.accept()
+    try:
+        while True:
+            message = await websocket.receive_text()
+            await websocket.send_text(message)
+    except:
+        pass
+
+app = Starlette(routes=[
+    Route("/", homepage),
+    WebSocketRoute("/ws", websocket_endpoint),
+])
